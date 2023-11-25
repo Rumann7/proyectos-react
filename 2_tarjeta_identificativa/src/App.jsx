727137b1acfca6b13ac641d5fb4df26cd2dc7e59
Rubén Manzano <rubenmanzanosanta@gmail.com>
2023-11-25 19:14:42 +0100

    0.2.0

--- 2_tarjeta_identificativa/src/App.jsx
+++ 2_tarjeta_identificativa/src/App.jsx
@@ -1,35 +1,102 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import styled from "@emotion/styled";
+
+const usersData = [
+  {
+    name: "EUGENIO",
+    surname: "RUIZ DOMENECH",
+    username: "eugenio_98",
+    gender: "male",
+    birthdate: "05/04/1998",
+    age: 25,
+    email: "eugenio_98@hotmail.com",
+    province: "Sevilla",
+    image: "https://randomuser.me/api/portraits/lego/1.jpg",
+  },
+  // Add more user data objects here...
+];
+
+const FlexContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+`;
+
+const generateRandomColor = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`;
+};
+
+const UserCard = styled.div`
+  width: 300px;
+  border-radius: 8px;
+  padding: 20px;
+  text-align: left;
+  border: 3px solid ${(props) => props.borderColor || "#ccc"};
+`;
+
+const Image = styled.img`
+  border-radius: 8px;
+  height: 130px;
+  width: 130px;
+`;
+
+const UserName = styled.div`
+  border-radius: 8px;
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+  background-color: ${(props) => props.backgroundColor || "white"};
+  color: black;
+  padding: 8px;
+  margin-bottom: 10px;
+`;
+
+const UserText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  margin-left: 10px;
+`;
+
+const Name = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const Provincia = styled.div`
+  font-size: 15px;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <FlexContainer>
+      {usersData.map((user, index) => {
+        const randomColor = generateRandomColor();
+        return (
+          <UserCard key={index} borderColor={randomColor}>
+            <UserName backgroundColor={randomColor}> {user.username}</UserName>
+            <div style={{ display: "flex" }}>
+              <Image src={user.image} alt={`${user.name} ${user.surname}`} />
+              <UserText>
+                <Name>{user.name}</Name>
+                <div>{user.surname}</div> <br />
+                <Provincia>{user.province}</Provincia>
+                <p style={{ fontSize: "10px" }}>
+                  {user.birthdate} ({user.age} años)
+                </p>
+              </UserText>
+            </div>
+            <p>{user.email}</p>
+          </UserCard>
+        );
+      })}
+    </FlexContainer>
+  );
 }
 
-export default App
+export default App;
